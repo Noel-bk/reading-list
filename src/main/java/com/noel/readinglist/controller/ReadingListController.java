5d5d772bea9e1d569ebe8e234a4b62a6bd22da07
Jaeyeong Baek <jaeyeong.b@gmail.com>
2017-08-05 00:18:18 +0900

    Clean up

--- src/main/java/com/noel/readinglist/controller/ReadingListController.java
+++ src/main/java/com/noel/readinglist/controller/ReadingListController.java
@@ -2,18 +2,18 @@ package com.noel.readinglist.controller;
 
 import com.noel.readinglist.entity.Book;
 import com.noel.readinglist.repository.ReadingListRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
+@Slf4j
 public class ReadingListController {
 
     @Autowired
@@ -32,6 +32,8 @@ public class ReadingListController {
     @PostMapping(value = "/{reader}")
     public String addToReadingList(@PathVariable("reader") String reader, Book book) {
         book.setReader(reader);
+
+        log.debug("book: {}", book);
         readingListRepository.save(book);
         return "redirect:/{reader}";
     }
--- src/main/java/com/noel/readinglist/entity/Book.java
+++ src/main/java/com/noel/readinglist/entity/Book.java
@@ -1,15 +1,13 @@
 package com.noel.readinglist.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-@Getter
-@Setter
+@Data
 public class Book {
 
     @Id
