516a0082bbfb1f7bca19917cd3dd5e5117fb211f
Jaeyeong Baek <jaeyeong.b@gmail.com>
2017-08-04 22:47:22 +0900

    Add entity

---
+++ src/main/java/com/noel/readinglist/controller/ReadingListController.java
@@ -0,0 +1,38 @@
+package com.noel.readinglist.controller;
+
+import com.noel.readinglist.entity.Book;
+import com.noel.readinglist.repository.ReadingListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class ReadingListController {
+
+    @Autowired
+    private ReadingListRepository readingListRepository;
+
+    @GetMapping(value = "/{reader}")
+    public String readersBooks(@PathVariable("reader") String reader, Model model) {
+
+        List<Book> readingList = readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+        }
+        return "readingList";
+    }
+
+    @PostMapping(value = "/{reader}")
+    public String addToReadingList(@PathVariable("reader") String reader, Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/{reader}";
+    }
+}
---
+++ src/main/java/com/noel/readinglist/entity/Book.java
@@ -0,0 +1,29 @@
+package com.noel.readinglist.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String reader;
+
+    private String isbn;
+
+    private String title;
+
+    private String author;
+
+    private String description;
+
+}
